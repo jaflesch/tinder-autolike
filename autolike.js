bc37f8658d4afc72a61b5767e1b777d3252ceb6f
Pedro Gomes <pedrogomesyoo@gmail.com>
2019-04-17 08:08:13 +0700

    Like profile depending on keywords blacklist

--- autolike.js
+++ autolike.js
@@ -1,11 +1,66 @@
+
+function hasBlacklistKeywords(bio) {
+		var blacklist = [
+			'ladyboy',
+			'lady boy',
+			'not a lady',
+			'chubby',
+			//' lb ',
+			'not lady'
+		];
+
+		blacklist.forEach(function(item){
+				if(bio.indexOf(item) !== -1) {
+					console.log('skipping profile, matched blacklist keyword ' + item);
+					return true;
+				} else {
+					return false;
+				}
+		});
+}
+
+function hasValidProfile() {
+		var bioClassName = 'profileCard__bio';
+		try {
+			var bio = document.getElementsByClassName(bioClassName)[0].textContent;
+			console.log(bio);
+			return !hasBlacklistKeywords(bio);
+		} catch (e) {
+			// console.log(e);
+			return true; // possible empty bio
+		}
+		return false;
+}
+
 function checkTinder() {
 	return window.location.href.startsWith("https://tinder.com/app/recs");
 }
 
+// prevent async execution
+function pause(milliseconds) {
+	var dt = new Date();
+	while ((new Date()) - dt <= milliseconds) { /* Do nothing */ }
+}
+
 function trickTinder() {
-	// Click, click, click the like button
-	var className = 'recsGamepad__button End(15px)';
-	document.getElementsByClassName(className)[0].click();
+
+	var infoClassName = 'recCard__info';
+	var dislike = document.getElementsByClassName("recsGamepad__button")[0];
+	var like = document.getElementsByClassName("recsGamepad__button")[2];
+
+	// Open profile bio
+	var info = document.getElementsByClassName(infoClassName)[0];
+	if(info) {
+			info.click();
+	}
+	pause(600);
+
+	// Like or deslike depending on validation
+	if(hasValidProfile()) {
+			like.click();
+	} else {
+			dislike.click();
+	}
 
 	// If reached max likes per day then show modal and get it's content...
 	// Check if there is any subscription button...
@@ -17,7 +72,7 @@ function trickTinder() {
 		// Minutes are worth 60 seconds. Hours are worth 60 minutes. 1 second = 1kmilliseconds.
 		// Genius... rocket science...
 		var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2])
-		
+
 		return seconds * 1000;
 	}
 }
@@ -34,7 +89,7 @@ function trickOkCupid() {
 // Need to add socket puppetry (VPNs solutions? several accounts?) - :D
 // TODO: Need to accept automatically permissions except for
 // TODO: Need to add ANN for fake pics
-// TODO: Need to add RNN for fake messages 
+// TODO: Need to add RNN for fake messages
 
 function getRandomPeriod() {
 	return Math.round(Math.random() * (2000 - 500)) + 500;
@@ -53,7 +108,7 @@ function getRandomPeriod() {
 			}
 		}
 		if (checkOkCupid()) {
-			trickOkCupid();  
+			trickOkCupid();
 		}
 		if (!randomPeriod) {
 			loopSasori();
